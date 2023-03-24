62eb5460fa70401ef9948ffed5334381d7853f57
madelagracia <marceladelagracia@gmail.com>
2023-03-24 15:32:22 -0300

    [3] Ofx Controller

---
+++ src/main/java/com/qyon/backend/controller/OfxController.java
@@ -0,0 +1,36 @@
+package com.qyon.backend.controller;
+
+import java.io.File;
+import java.io.InputStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import net.sf.ofx4j.domain.data.RequestEnvelope;
+import net.sf.ofx4j.io.AggregateUnmarshaller;
+
+@RestController
+public class OfxController {
+
+     @PostMapping("/upload")
+     public String fileUpload(@RequestParam("file") MultipartFile file) {
+
+        AggregateUnmarshaller<RequestEnvelope> unmarshaller = new AggregateUnmarshaller<RequestEnvelope>(
+            RequestEnvelope.class);
+        
+        try {
+            InputStream inputStream = file.getInputStream();
+            RequestEnvelope envelope = unmarshaller.unmarshal(inputStream);
+
+            File ofx = new File("C:\\ofx\\" + envelope.getUID() + ".ofx");
+            file.transferTo(ofx);
+
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+
+        return "ok";
+    }
+}
